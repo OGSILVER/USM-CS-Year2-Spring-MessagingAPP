6a92a3cff811625874cbd78831284bf369f108b4
Skirtty <skirttyone@gmail.com>
2026-03-01 17:34:33 +0200

    new file:   .gitattributes 	new file:   .gitignore 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/dev/skirtty/webmessaging/WebmessagingApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/dev/skirtty/webmessaging/WebmessagingApplicationTests.java

---
+++ src/main/java/dev/skirtty/webmessaging/WebmessagingApplication.java
@@ -0,0 +1,13 @@
+package dev.skirtty.webmessaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebmessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebmessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/skirtty/webmessaging/WebmessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.skirtty.webmessaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebmessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
